316efb5ead2b002c4258d9647ca631bf776c8fda
acappon <andrew.cappon@gmail.com>
2019-02-10 23:53:43 -0800

    .

--- test_cv_eclipse/src/frc/robot/JReceiver.java
+++ test_cv_eclipse/src/frc/robot/JReceiver.java
@@ -4,14 +4,9 @@
  */
 package frc.robot;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
